3eb2bc404c8913e0ffcee46b9794e4258061217b
maxxedtech <davidadamsonshaffa@gmail.com>
2026-03-04 20:03:46 +0100

    Update AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,17 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "@/components/Navbar";
 
 export default function AppLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-
       <main className="flex-1">
         <Outlet />
       </main>
-
-      <Footer />
     </div>
   );
 }
