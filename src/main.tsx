881d1b98dee3fad4449f09dd044b8b1635e8a1a7
maxxedtech <davidadamsonshaffa@gmail.com>
2026-03-06 13:01:56 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
-
 import { AuthProvider } from "@/contexts/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
