da5b4c1523b8e4919806e54bf167a933d460e70f
maxxedtech <davidadamsonshaffa@gmail.com>
2026-03-19 21:06:17 +0100

    Update firebase.ts

--- src/src/firebase.ts
+++ src/src/firebase.ts
@@ -1,7 +1,8 @@
+// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// your config (from Firebase)
 const firebaseConfig = {
   apiKey: "AIzaSyAHNxf-GFW8G-4GvcCYQ9JeDT_STa1Zv9Y",
   authDomain: "campusprenuer-4a36b.firebaseapp.com",
@@ -9,11 +10,10 @@ const firebaseConfig = {
   storageBucket: "campusprenuer-4a36b.firebasestorage.app",
   messagingSenderId: "678248714267",
   appId: "1:678248714267:web:fcea748fce992fe33ca8be",
-  measurementId: "G-Z28M3T22KN"
+  measurementId: "G-Z28M3T22KN",
 };
 
-// initialize firebase
 const app = initializeApp(firebaseConfig);
-
-// connect to database
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
