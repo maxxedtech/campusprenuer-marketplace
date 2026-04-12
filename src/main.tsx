50375e0931f5ad2b0752e965064d45337233db53
maxxedtech <davidadamsonshaffa@gmail.com>
2026-04-12 10:33:20 +0100

    Add Toaster component to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "@/contexts/AuthContext";
@@ -7,6 +8,7 @@ import { AuthProvider } from "@/contexts/AuthContext";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AuthProvider>
+      <Toaster position="top-right" />
       <App />
     </AuthProvider>
   </BrowserRouter>
