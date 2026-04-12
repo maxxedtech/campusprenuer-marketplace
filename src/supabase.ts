ce677281c7701bf68b6470f95c876b6213545357
maxxedtech <davidadamsonshaffa@gmail.com>
2026-04-12 10:28:18 +0100

    Update Supabase client initialization to use Vite env

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,8 @@
 // src/supabase.ts
 
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://cmheobhagkyrcuqjcjle.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey) 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
