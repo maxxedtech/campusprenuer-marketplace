12cce4ec2d61f62b753b6f98c7e2de5184562341
maxxedtech <davidadamsonshaffa@gmail.com>
2026-04-12 10:32:09 +0100

    Add Loader component for loading state

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="animate-pulse text-gray-500">Loading...</p>
+    </div>
+  );
+}
